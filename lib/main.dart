ca5208a2d76eea9c2b001f60ab3bb74ad33b5577
Messi002 <rostandmessi2@gmail.com>
2022-07-20 20:25:04 +0100

    weatherApp

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,3 @@ class MyApp extends StatelessWidget {
 }
 
 
-import 'package:flutter/material.dart';
-import 'package:precipatation/screens/loading_screen.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: LoadingScreen(),
-    );
-  }
-}
