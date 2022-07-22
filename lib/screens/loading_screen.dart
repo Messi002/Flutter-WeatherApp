9eb2d3ec668eeac755c6f7383408d207e15eedbb
Messi002 <rostandmessi2@gmail.com>
2022-07-22 22:47:56 +0100

    weatherApp

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,10 @@
 // ignore_for_file: library_private_types_in_public_api, avoid_print
 
-import 'dart:convert';
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:precipatation/screens/location_screen.dart';
 import 'package:precipatation/services/location.dart';
-import 'package:http/http.dart' as http;
 import 'package:precipatation/utilities/constants.dart';
+import 'package:precipatation/services/networking.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -15,8 +14,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-    double? latitude;
-    double? longitude;
+  double? latitude;
+  double? longitude;
 
   @override
   void initState() {
@@ -24,40 +23,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
     getLocation();
   }
 
-
-    void getLocation() async {
+  void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
     latitude = location.latitude;
-    longitude =location.longitude;
-    getData();
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kapiKey');
+
+    var weather = await networkHelper.getData();
+
+    // ignore: use_build_context_synchronously
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(),
+      ),
+    );
   }
-
-  Future<void> getData() async {
-     final uri = Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kapiKey');
-    //  final uri = Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=London&appid=$kapiKey');
-   
-      
-      final response = await http.get(uri);
-
-      if( response.statusCode==200){
-           String data = response.body;
-      var decodeData = jsonDecode(data);
-
-      double temperature = decodeData['main']['temp'];
-      int condition = decodeData['weather'][0]['id'];
-      String cityName = decodeData['name'];
-
-      print(temperature);
-      print(condition);
-      print(cityName);
-      }else {
-      print(response.statusCode);
-     }
-     
-  }
-
-
 
   @override
   Widget build(BuildContext context) {
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,25 +1,24 @@
 import 'package:http/http.dart' as http;
-import 'package:precipatation/utilities/constants.dart';
 import 'dart:convert';
 import 'dart:async';
 
 
 class NetworkHelper{
 
-    NetworkHelper(this.url);
+    NetworkHelper(this.uri);
 
-    final String url;
+    final String uri;
 
-    void getData() async{
-         final uri = Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kapiKey');
+    Future<void> getData() async{
     //  final uri = Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=London&appid=$kapiKey');
    
       
-      final response = await http.get(uri);
+      final response = await http.get(Uri.parse(uri));
 
       if( response.statusCode==200){
            String data = response.body;
       var decodeData = jsonDecode(data);
+      return decodeData;
       }else {
       print(response.statusCode);
      }
